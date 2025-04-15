31dd618de848c5c8b3ebcb206e7dd33b36e2130f
Alex Mateo <jdv.escobar@yavirac.edu.ec>
2025-04-14 21:55:24 -0500

    Subir Proyecto clase 21

---
+++ frontend_3b-master/src/app/admin/inventario/components/producto/producto.component.ts
@@ -0,0 +1,98 @@
+import { Component, inject } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+import { UploadEvent } from 'primeng/fileupload';
+import { FileUploadHandlerEvent } from 'primeng/fileupload';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.scss'
+})
+export class ProductoComponent {
+
+  private productoService = inject(ProductoService)
+
+  loading:boolean=false;
+  totalRecords!:number;
+  buscador:string='';
+  visible:boolean=false;
+  producto_id:number=-1;
+
+  categorias: any = [
+    { name: 'Ropa Dama', code: 'RD' },
+    { name: 'Ropa Caballero', code: 'RC' },
+    { name: 'Herramientas', code: 'He' },
+    { name: 'Tecnologia', code: 'Tec' },
+    { name: 'Hogar', code: 'Hgr' }
+  ]
+
+  products: any[] = [];
+  cols: any[] = [];
+
+  constructor() {
+    this.productoService.funListar().subscribe(
+      (res: any) => {
+        this.products = res.data
+      }
+    )
+  }
+
+  loadProductos(event:any){
+    let page=(event.first/event.rows)+1
+    this.listar(page,event.rows)
+    console.log(event)
+  }
+
+  listar(page=1, limit=10, q=''){
+    this.productoService.funListar(page,limit,q).subscribe(
+      (res:any)=>{
+        this.products=res.data;
+        this.totalRecords=res.total;
+        this.loading=false;
+      }
+    )
+  }
+  buscar(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.listar(1, 10, this.buscador);
+    } else if (this.buscador === '') {
+      this.listar(); 
+    }
+  }
+  
+
+  openNew() {
+
+  }
+
+  editProduct(prod: any) {
+
+  }
+
+  deleteProduct(prod: any) {
+
+  }
+
+  uploadedFiles:any[]=[];
+  subirImagen(event:any){
+    console.log(event.files[0])
+    let formData=new FormData();
+    formData.append("imagen", event.files[0])
+
+    this.productoService.actualizarImagen(formData, this.producto_id).subscribe(
+      (res:any)=>{
+        console.log(res);
+        this.visible=false;
+        this.producto_id=-1;
+        this.listar();
+      }
+    )
+  }
+
+  openDialogImagen(id:number){
+    this.visible=true
+    this.producto_id=id
+  }
+
+
+}
